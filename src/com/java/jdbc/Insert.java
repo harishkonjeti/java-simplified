d6fa2ac261357272f4323638d5bff614f8b3e0f4
Hareesh Konjeti <harishkonjeti34@gmail.com>
2023-03-27 00:15:16 +0530

    Prepared statement for dynamic values

--- src/com/java/jdbc/Insert.java
+++ src/com/java/jdbc/Insert.java
@@ -12,17 +12,26 @@ public class Insert {
         String url ="jdbc:mysql://localhost:3306/hareesh";
         String username = "root";
         String password = "root";
-        String query = "insert into student values(5, \"Arjun\");";
-        Class.forName("com.mysql.cj.jdbc.Driver");
-//        com.mysql.jdbc.Driver.class
-    
+        
+        int rollno = 6;
+        String studentname = "vijay";
+        String query = "insert into student values(?,?);";
+        Class.forName("com.mysql.cj.jdbc.Driver");    
+                
+        Connection con = DriverManager.getConnection(url, username, password);
         /*
+         * com.mysql.jdbc.Driver.class
          * select studentname from student where rollno=4;
+         * insert into student values(4, "Ram");
+         * Statement st = con.createStatement();
+         * int count = st.executeUpdate(query);
+         * for adding dynamic values to DB we use Prepared Statement
          */
         
-        Connection con = DriverManager.getConnection(url, username, password);
-        Statement st = con.createStatement();
-        int count = st.executeUpdate(query);
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, rollno);
+        st.setString(2, studentname);
+        int count = st.executeUpdate();
         System.out.println(count+" row/s affected");
         
         st.close();
